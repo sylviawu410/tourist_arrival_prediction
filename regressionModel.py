28b93c09490ba5204dd475785963407cd7f9dbb3
Sylvia <sylvia.wu.wmy410@gmail.com>
2024-04-15 21:18:25 +0800

    plotted graphs, added a database

--- regressionModel.py
+++ regressionModel.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression 
 
 # import csv data sets
-# pd.set_option('display.max_columns', None)
 
 # CPI
 CPIfile = pd.read_csv('databases/monthlyCPI.csv')
-CPIdata = CPIfile.iloc[136:522, 2] # Retrieve data from a specific row and column 
+CPIdata = CPIfile.iloc[256:520, 2] # Retrieve data from a specific row and column 
+print(CPIdata)
 
 # GDP
 GDPfile = pd.read_csv('databases/HKGDP.csv')
@@ -18,16 +18,23 @@ GDPdata = GDPfile.iloc[4, 141:269]
 
 
 # Latest local situation of COVID-19
-COVIDfile = pd.read_csv('databases/local_situation_COVID-19.csv')
-COVIDdata = COVIDfile.iloc[4, 141:269]
+COVIDfile = pd.read_csv('databases/local_covid_situation.csv')
+COVIDdata = COVIDfile.iloc[0:26, 1]
+
+# Average Hotel Room Occupancy Rate
+hotelFile = pd.read_csv("databases/hotel_room_occupancy_rate_monthly.csv")
+hotelData = hotelFile.iloc[0:264,1]
 
 # Number of visitor arrival
 visitorFile = pd.read_csv('databases/number_of_visitor_arrival.csv')
-visitorData = visitorFile.iloc[1:265,1]
-print(visitorData)
+visitorData = visitorFile.iloc[0:265,1]
 
-# plt.scatter(area, price)
-# plt.xlim(0, 1500)
-# plt.xlabel("SaleableArea (ft)")
-# plt.ylabel("SalePrice (10k hkd)")
-# plt.show()
+
+# pd.set_option('display.max_columns', None)
+
+# check correlation
+plt.scatter(CPIdata, visitorData)
+plt.xlabel("CPI")
+plt.ylabel("Number of visitor arrival")
+plt.yscale('log') 
+plt.show()
