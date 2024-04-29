d05e18dce538bbd912cae952fd5b32d793b3d1b7
Sylvia <sylvia.wu.wmy410@gmail.com>
2024-04-29 14:07:00 +0800

    update dataProcessing

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,26 +1,20 @@
-# this file handle the datasets by creating the dataframe and output the correlation figure
+# this file handle the datasets by creating the dataframe and output the correlation figures
 
 # Import python libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression 
 
 # import csv data sets
-
 # CPI 
 CPIfile = pd.read_csv('databases/monthlyCPI.csv')
 CPIdata = CPIfile.iloc[256:520, 2] 
 CPIdata = pd.to_numeric(CPIdata)
 
-# GDP
+# GDP (the data is already seasonal)
 GDPfile = pd.read_csv('databases/HKGDP.csv')
 GDPdata_seasonal = GDPfile.iloc[4, 181:269]
 GDPdata_seasonal = GDPfile.iloc[4, 181:269].values.reshape(-1, 1)
-
-
-# GDPdata_seasonal = GDPdata_seasonal.values.reshape(-1,)
-# print(GDPdata_seasonal.shape)
 
 # pandemic_diseases_HK(SARS and COVID-19)
 pandemicFile = pd.read_csv('databases/pandemics_in_HK.csv')
@@ -33,6 +27,7 @@ hotelData = hotelFile.iloc[0:264,1]
 # Number of visitor arrival
 visitorFile = pd.read_csv('databases/number_of_visitor_arrival.csv')
 visitorData = visitorFile.iloc[0:265,1]
+
 
 # Convert the monthly datasets to seasonal data
 index = pd.date_range('2002-01-01', periods=264, freq='M')
@@ -47,12 +42,14 @@ pandemicData_seasonal = pandemicData.resample('Q').mean()
 hotelData_seasonal = hotelData.resample('Q').mean()
 visitorData_seasonal = visitorData.resample('Q').mean()
 
-indexGDP = pd.date_range('2002-01-01', periods=88, freq='Q')
-GDPdata_seasonal = pd.DataFrame(GDPdata_seasonal, columns=['GDP'])
-GDPdata_seasonal = GDPdata_seasonal.set_index(indexGDP)
 
-combined_data = pd.concat([CPIdata_seasonal, pandemicData_seasonal, hotelData_seasonal, visitorData_seasonal, GDPdata_seasonal], axis=1)
-combined_data.to_csv('dataFrame2.csv', index=False)
+def create_df():
+    indexGDP = pd.date_range('2002-01-01', periods=88, freq='Q')
+    GDPdata_seasonal = pd.DataFrame(GDPdata_seasonal, columns=['GDP'])
+    GDPdata_seasonal = GDPdata_seasonal.set_index(indexGDP)
+
+    combined_data = pd.concat([CPIdata_seasonal, pandemicData_seasonal, hotelData_seasonal, visitorData_seasonal, GDPdata_seasonal], axis=1)
+    combined_data.to_csv('dataFrame.csv', index=False)
 
 def check_dataset_shape():
     print(CPIdata.shape)
@@ -96,6 +93,12 @@ def correlation_hotel():
     plt.yscale('log') 
     plt.show()
 
-
+def correlation_GDP():
+    GDPdata_seasonal_series = pd.Series(GDPdata_seasonal.flatten(), index=visitorData_seasonal.index)
+    plt.scatter(GDPdata_seasonal_series, visitorData_seasonal)
+    plt.xlabel("Seasonal GDP")
+    plt.ylabel("Seasonal number of visitor arrival")
+    plt.xscale('log') 
+    plt.show()
 
 # pd.set_option('display.max_columns', None)
